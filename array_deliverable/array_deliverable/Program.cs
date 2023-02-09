f47eb77414d1a353f2a150a4e4f0f8b7a220a783
hai <101365274+hloub@users.noreply.github.com>
2023-02-09 11:27:59 -0500

    Add files via upload

---
+++ array_deliverable/array_deliverable/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace array_deliverable;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] fibonacci = new int[25];
+
+        fibonacci[0] = 1;
+        fibonacci[1] = 1;
+
+    
+
+        for (int i = 2; i < fibonacci.Length; i++) 
+        {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        Console.WriteLine("Fibnacci number " + fibonacci.Length);
+
+        for (int i = 0; i < fibonacci.Length; i++)
+        {
+            Console.Write(" " + fibonacci[i]);
+        }
+        
+            
+    }
+}
+
